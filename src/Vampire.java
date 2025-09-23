d53c9324abab753e246a56d3fa6021d3b4a3a344
Aidan Mora <aidanmora2004@gmail.com>
2025-09-22 21:49:06 -0400

    Added the boolean status for if the Vampire is transformed or not.
    Updated the attack() method with the if statement to check the status of the vampire, this will change which attack the vampire will use.

--- src/Vampire.java
+++ src/Vampire.java
@@ -1,19 +1,28 @@
 public class Vampire extends HorrorCharacter {
+    private boolean transformed;
+
     public Vampire(String name, int health, Vulnerability[] vulnerabilities) {
         super(name, health, new Vulnerability[]{
                 Vulnerability.CRUCIFIX,
-                Vulnerability.SILVER,
-                Vulnerability.SUNLIGHT
-        });
+                Vulnerability.SUNLIGHT});
+        transformed = false;
     }
     @Override
     public void attack() {
-        System.out.println("The " + name + " is trying to bite your neck!");
+        if (!transformed){
+            System.out.println("The " + name + " is trying to bite your neck!");
+        }else {
+            System.out.println("The " + name + " turned into a bat and is clawing" +
+                    "at your eyes with every swoop!");
     }
+        }
 
     @Override
     public void flee() {
-        System.out.println("You turn and run away from the " + name);
+        System.out.println("You turn and run away from the " + name +
+                " (it turned into a bat and transformed " +
+                "back to a vampire in front of you and drains " +
+                "you of all your blood! >:) ");
     }
 
 }

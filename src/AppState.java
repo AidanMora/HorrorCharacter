9354de76783f568d3fade345325c76156783285e
Aidan Mora <aidanmora2004@gmail.com>
2025-11-11 23:59:05 -0500

    - Updated EVERYTHING in AppState.java.
    Only one list that will be maintained (observableList)
    
    Now both my scenes can communicate with eachother using this AppState.java file
    
    Contains "fake" or default data that is read from my fake database

--- src/AppState.java
+++ src/AppState.java
@@ -1,2 +1,40 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class AppState {
+    private static final AppState instance = new AppState();
+    public static AppState getInstance() { //fetch current instance
+        return instance;
+    }
+    private final ObservableList<HorrorCharacter> characters = FXCollections.observableArrayList(); //my list of character objects
+    //best for the ListView javafx stuff
+    private HorrorCharacter currCharacter; //current character that can be edited or removed
+    private AppState(){
+        //fake "Read" data or just default data
+        characters.addAll(
+                new Zombie("Bob", 200, new Vulnerability[]{
+                        Vulnerability.BLUNT_DAMAGE,
+                        Vulnerability.BULLET
+                }),
+                new Werewolf("Dave", 300, new Vulnerability[]{
+                        Vulnerability.WOLFSBANE
+                }, true),
+                new Vampire("Eric", 400, new Vulnerability[]{
+                        Vulnerability.SUNLIGHT
+                }, false)
+        );
+    }
+
+    //getters and setters so my primary controllers can get or set data
+    public ObservableList<HorrorCharacter> getCharacters(){
+        return characters;
+    }
+
+    public HorrorCharacter getCurrCharacter(){
+        return currCharacter;
+    }
+
+    public void setCurrCharacter(HorrorCharacter currCharacter){
+        this.currCharacter = currCharacter;
+    }
 }
